e4896411cbceda72fba5491920984b5a9031c2d3
Hrishikesh <rishibodake@gmail.com>
2020-04-15 21:09:54 +0530

    Added:Class CabInvoiceGenerator and Test for Checking Fare

---
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest
+{
+    //Test For Calculating Fare
+    @Test
+    public void givenDistanceAndTime_InvoiceGenerator_shouldReturnTrue()
+    {
+
+    }
+}
